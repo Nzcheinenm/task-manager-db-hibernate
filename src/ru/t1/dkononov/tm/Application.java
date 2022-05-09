adf6f46babee5d79801faba4051fec08eb430555
Dmitry <koliasam1@gmail.com>
2022-05-09 12:41:07 +0300

    Добавлена логика обработки аргументов и отдельный класс с константами.

--- src/ru/t1/dkononov/tm/Application.java
+++ src/ru/t1/dkononov/tm/Application.java
@@ -1,8 +1,59 @@
 package ru.t1.dkononov.tm;
 
+import static ru.t1.dkononov.tm.constant.TerminalConst.*;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        processArguments(args);
     }
+
+    public static void processArguments(final String[] args) {
+        if (args == null || args.length == 0) return;
+        final String argument = args[0];
+        processArgument(argument);
+    }
+
+    private static void processArgument(final String argument) {
+        if (argument == null || argument.isEmpty()) return;
+        switch (argument) {
+            case HELP:
+                showHelp();
+                break;
+            case ABOUT:
+                showAbout();
+                break;
+            case VERSION:
+                showVersion();
+                break;
+            default:
+                showError();
+                break;
+        }
+    }
+
+    private static void showHelp() {
+        System.out.println("[HELP]");
+        System.out.printf("%s - Показать информацию о разработчике.\n",ABOUT);
+        System.out.printf("%s - Показать версию приложения.\n",VERSION);
+        System.out.printf("%s - Показать справку о командах.\n",HELP);
+    }
+
+    private static void showVersion() {
+        System.out.println("[VERSION]");
+        System.out.println("1.2.0");
+    }
+
+    private static void showAbout() {
+        System.out.println("[ABOUT]");
+        System.out.println("имя: Дмитрий Кононов");
+        System.out.println("email: dkononov@t1-consulting.com");
+    }
+
+    private static void showError() {
+        System.err.println("[Error]");
+        System.err.println("Этот аргумент не поддерживается...");
+    }
+
 }
+
---
+++ src/ru/t1/dkononov/tm/constant/TerminalConst.java
@@ -0,0 +1,9 @@
+package ru.t1.dkononov.tm.constant;
+
+public final class TerminalConst {
+
+    public static final String ABOUT = "about";
+    public static final String VERSION = "version";
+    public static final String HELP = "help";
+
+}
