cf6b2c7796761d915b66e0171df51a2e23562dd3
Dmitry <koliasam1@gmail.com>
2022-05-19 22:31:30 +0300

    Добавлена поддержка ApacheMaven.

---
+++ src/test/java/ru/t1/dkononov/tm/ApplicationTest.java
@@ -0,0 +1,14 @@
+package ru.t1.dkononov.tm;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
